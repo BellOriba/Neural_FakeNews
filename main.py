35a75af4ed9b2ad5a9f82c9774b933d4f6d9b26b
Gabriel Oliveira <62845480+GGos-Bell@users.noreply.github.com>
2024-04-27 00:01:33 -0300

    Dataset Reading

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Main file
+import csv
+
+with open("./data/fake_and_real_news.csv", mode='r', encoding='utf-8') as file:
+    data = csv.reader(file)
+    # [Text, label]
+    for row in data:
+        print(row[1])
+
